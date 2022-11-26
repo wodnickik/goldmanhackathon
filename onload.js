85f19e178b3e2b8ebb513751b9a7096e35681284
Filip-Sz <filipszymplinski@gmail.com>
2022-11-26 08:37:13 +0100

    the end

--- onload.js
+++ onload.js
@@ -1,2 +1,17 @@
-let x = sessionStorage.getItem("uni")
-alert(x);
+console.log(sessionStorage.getItem("x"));
+if(sessionStorage.getItem("offer1") === "nie") {
+    document.getElementById("box1").checked = true;
+    document.getElementById("box2").checked = true;
+    document.getElementById("box3").checked = true;
+    document.getElementById("box4").checked = true;
+}
+else{
+    document.getElementById("box1").checked = true;
+    document.getElementById("box3").checked = true;
+    let elem = document.getElementById("job2");
+    elem.style.height = 0 + "px";
+    elem.style.visibility = "hidden";
+    let elem2 = document.getElementById("job4");
+    elem2.style.height = 0 + "px";
+    elem2.style.visibility = "hidden";
+}
--- save_form.js
+++ save_form.js
@@ -1,4 +1,15 @@
 function save_form() {
-    let x = document.getElementById("uni_name").value;
-    sessionStorage.setItem("uni", x);
+    let x = document.getElementById("python").checked;
+    if(x == false){
+        sessionStorage.setItem("x", "nie");
+    }
+    else{
+        sessionStorage.setItem("x", "tak");
+    }
+    console.log(sessionStorage.getItem("x"));
+    // if(x === false){
+    //     localStorage.setItem("offer1", "nie");
+    // }
+    // console.log(localStorage.getItem("offer1"));
+    // window.location.href = "./jobs/jobs.html";
 }
