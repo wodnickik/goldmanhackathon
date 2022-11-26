0cb5edbd610bc237747fdb051daabe2490f5e1a7
Krzysztof Wodnicki <woodoo650@gmail.com>
2022-11-26 06:03:07 +0100

    submitting form

--- result.html
+++
@@ -1,1 +0,0 @@
-<h2>Search results</h2>
--- submit.js
+++
@@ -1,7 +0,0 @@
-let submitButton = document.getElementById("submit");
-
-submitButton.addEventListener("click", function() {
-    let quiz = document.getElementById("quiz");
-
-    quiz.innerHTML = '<object type="text/html" data="result.html" ></object>';
-});
